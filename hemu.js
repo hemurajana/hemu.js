f5253c21ce8e15bca9b3847bfb59205e151fa8a2
hemurajana <163818549+hemurajana@users.noreply.github.com>
2024-04-03 12:24:28 +0530

    Add files via upload

---
+++ hemu.js
@@ -0,0 +1,7 @@
+let username;
+document.getElementById("bth").onclick function(){
+username=document.getElementById("T2")Value;
+document.getElementById("myh2").textContent='hello' ${username};//${}
+console.log(username);
+
+}
